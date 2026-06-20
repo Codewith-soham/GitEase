d39ac18fb074e19cfdfa9f56557f69afb37dc852
Codewith-soham <ghadgesoham2006@gmail.com>
2026-06-21 03:20:10 +0530

    Creating github service

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,37 @@
-import 
+import mongoose  from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    githubId: {
+        type: String,
+        required: true,
+        lowercase: true,
+        index: true,
+        unique: true
+    },
+
+    username : {
+        type: String,
+        lowercase: true,
+        index: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        lowercase: true
+    },
+
+    avatar: {
+        type: String
+    },
+
+    githubAccessToken:{
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true
+})
+
+export const User = mongoose.model('User' , userSchema)
