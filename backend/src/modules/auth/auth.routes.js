493eb1abb02a6e1d6b4f5a8721756c5b29ead49a
Codewith-soham <ghadgesoham2006@gmail.com>
2026-06-27 11:10:26 +0530

    added getallsession routing

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { redirectToGithub } from './auth.controller.js'
+import { getAllSessions, redirectToGithub } from './auth.controller.js'
 import { handleGithubCallBack, getMe } from './auth.controller.js'
 import { verifyJwt } from "./auth.middleware.js"
 import { logOut , logOutAll , newAccessToken} from './auth.controller.js'
@@ -12,5 +12,6 @@ router.route("/me").get(verifyJwt, getMe)
 router.route("/logout").post(verifyJwt , logOut)
 router.route("/logoutall").post(verifyJwt, logOutAll)
 router.route("/refresh-token").post(newAccessToken)
+router.route("/sessions").get(verifyJwt, getAllSessions)
 
 export default router 
